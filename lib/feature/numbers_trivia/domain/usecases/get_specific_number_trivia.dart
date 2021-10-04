508bc06a097da18b82f43f87bd252d0cad2a30d6
FarahAbuthaher <69241754+FarahAbuthaher@users.noreply.github.com>
2021-10-04 15:51:48 +0300

    Add files via upload

---
+++ lib/feature/numbers_trivia/domain/usecases/get_specific_number_trivia.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:number_trivia/core/error/failure.dart';
+import 'package:number_trivia/core/usecase/usecase.dart';
+import 'package:number_trivia/feature/numbers_trivia/domain/entities/number_trivia.dart';
+import 'package:number_trivia/feature/numbers_trivia/domain/repositories/number_trivia_repository.dart';
+
+// in usecase we create classes to use the business objects (i.e business logic);
+
+class GetSpecificNumberTrivia extends UseCase<NumberTrivia,Params> {
+  final NumberTriviaRepository repository;
+  GetSpecificNumberTrivia(this.repository);
+
+  Future<Either<Failure, NumberTrivia>> call(Params params)
+  async {
+    return await repository.getSpecificNumberTrivia(params.number);
+  }
+}
+
+// no real logic, just getting data from Repo
+
+class Params extends Equatable{
+  final int number;
+
+  Params(this.number);
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [Params(number)];
+
+}

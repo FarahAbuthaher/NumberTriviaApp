69fccf5e3170ac7aa92b709be503ec3881713b38
FarahAbuthaher <69241754+FarahAbuthaher@users.noreply.github.com>
2021-10-06 15:38:09 +0300

    Delete lib directory

--- lib/feature/numbers_trivia/data/dataSources/number_trivia_remote_dataSource.dart
+++
@@ -1,8 +0,0 @@
-import 'package:number_trivia/feature/numbers_trivia/data/models/number_trivia_model.dart';
-
-// errors will be handled by exceptions unlike in Repo
-// contract
-abstract class NumberTriviaRemoteDataSource {
-   Future<NumberTriviaModel> getSpecificNumberTrivia(int number);
-   Future<NumberTriviaModel> getRandomNumberTrivia();
-}
